b14771aabc29f1b164a3e075114b3657c9f04b23
Fikri Can Cankurtaran <fikrican.cankurtaran@solidict.com>
2017-11-30 10:46:28 +0300

    set dummy location when we cant get a cooardinate

--- merakly-ios/Main Class/Merakly.swift
+++ merakly-ios/Main Class/Merakly.swift
@@ -18,17 +18,25 @@
         
         self.locationManager.fetchWithCompletion { (location, err) in
             
-            guard let latitude = location?.coordinate.latitude else { return }
-            guard let longitude = location?.coordinate.longitude else { return }
-
-            identifier = MRKIdentifier(apiKey: apiKey, secretKey: appSecret, latitude: latitude, longitude: longitude)
-            
-            do {
-                let identifierData = try JSONSerialization.data(withJSONObject: identifier.marshaled(), options: JSONSerialization.WritingOptions.prettyPrinted)
-                let identifierBase64 = identifierData.base64EncodedString()
-                MRKAPIRouter.identifierBase64 = identifierBase64
-            } catch {
-                print("Could not create JSON from identifier")
+            if err == nil {
+                
+                guard let latitude = location?.coordinate.latitude else { return }
+                guard let longitude = location?.coordinate.longitude else { return }
+                
+                identifier = MRKIdentifier(apiKey: apiKey, secretKey: appSecret, latitude: latitude, longitude: longitude)
+                
+                do {
+                    let identifierData = try JSONSerialization.data(withJSONObject: identifier.marshaled(), options: JSONSerialization.WritingOptions.prettyPrinted)
+                    let identifierBase64 = identifierData.base64EncodedString()
+                    MRKAPIRouter.identifierBase64 = identifierBase64
+                } catch {
+                    print("Could not create JSON from identifier")
+                }
+                
+            }else {
+                
+                identifier = MRKIdentifier(apiKey: apiKey, secretKey: appSecret, latitude: 0.0, longitude: 0.0)
+                
             }
             
         }
